afcc98f9d3e66192d871eb7d28d0e928fc353841
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-06-15 22:04:22 +0500

    feat: added navbar starter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { HomeComponent } from './modules/home/home.component';
+import { CollectionComponent } from './modules/collection/collection.component';
+import { ItemComponent } from './modules/item/item.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignUpComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'collection', component: CollectionComponent },
+  { path: 'item', component: ItemComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userId: any;
+  darkMode: any;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.userId = localStorage.getItem('userId');
+    this.darkMode = localStorage.getItem('darkMode');
+  }
 }
